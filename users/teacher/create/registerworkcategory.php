9f1290321a8ca3f0f75258a7b8732ee63d23a1c4
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-05 18:12:17 -0300

    Revisão semântica

--- users/teacher/create/registerworkcategory.php
+++ users/teacher/create/registerworkcategory.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar Nível Atividade</title>
+    <title>Registrar Categoria Atividade</title>
 </head>
 <body>
     <form action="../../../includes/create.php" method="post">

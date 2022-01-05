e4114d2307d1d768a2ebf6a7a75cb02b89932067
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-05 17:36:37 -0300

    Adicionado exclusão de atividades

--- includes/delete.php
+++ includes/delete.php
@@ -15,10 +15,25 @@
         }
     }
     
-    function deleteactivitycategory(){
+    function deleteworkcategory(){
         require_once './db_connection.php';
         $id = mysqli_escape_string($connect, $_POST['id']);
         $sql = "DELETE FROM categoriaatividade WHERE idcategoriaAtividade = '$id';";
+
+        if(mysqli_query($connect, $sql)){
+            $_SESSION['mensagem']= "Deletado com sucesso!";
+            header('Location: ../users/teacher/index.php');
+        }else{
+            $_SESSION['mensagem']= "Erro ao deletar!";
+            echo mysqli_error($connect);
+            // header('Location: ../users/teacher/index.php');
+        }
+    }
+
+    function deletework(){
+        require_once './db_connection.php';
+        $id = mysqli_escape_string($connect, $_POST['id']);
+        $sql = "DELETE FROM atividade WHERE idatividade = '$id';";
 
         if(mysqli_query($connect, $sql)){
             $_SESSION['mensagem']= "Deletado com sucesso!";
@@ -35,7 +50,10 @@
             deleteworklevel();
             break;
         case 'deletarCategoria':
-            deleteactivitycategory();
+            deleteworkcategory();
+            break;
+        case 'deletarAtividade':
+            deletework();
             break;
         default:
         echo "Não foi possível realizar a operação!";

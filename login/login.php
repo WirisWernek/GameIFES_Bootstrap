dfa3457532b02350dee56d2cf8ddf93cf8f41746
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-04 23:32:50 -0300

    Adionado funcionalidades de login com redirecionamento

--- login/login.php
+++ login/login.php
@@ -7,13 +7,43 @@ if(isset($_POST['btn-login'])){
     
     $sql = "SELECT * FROM usuario WHERE login = '$login' AND senha = '$senha';";
 
-    if(mysqli_query($connect, $sql)){
-        if(mysqli_num_rows(mysqli_query($connect, $sql)) > 0){
-            $_SESSION['mensagem']= "Login efetuado com sucesso!";
+    if($resultado = mysqli_query($connect, $sql)){
+        if(mysqli_num_rows($resultado) > 0){
+            // $resultado = mysqli_query($connect, $sql);
+            $dados = mysqli_fetch_assoc($resultado);
+            switch($dados['perfilUsuarioID']){
+                case '1':
+                    $_SESSION['id'] = $dados['idusuario'];
+                    $_SESSION['nome'] = $dados['nome'];
+                    $_SESSION['tipo'] = $dados['tipo'];
+                    $_SESSION['mensagem'] = "Login efetuado com sucesso!";
+                    header('Location: ../users/admin/index.php');
+                    break;
+                case '2':
+                    $_SESSION['id'] = $dados['idusuario'];
+                    $_SESSION['nome'] = $dados['nome'];
+                    $_SESSION['tipo'] = $dados['tipo'];
+                    $_SESSION['mensagem'] = "Login efetuado com sucesso!";
+                    header('Location: ../users/user/home/index.php');
+                    break;
+                case '3':
+                    $_SESSION['id'] = $dados['idusuario'];
+                    $_SESSION['nome'] = $dados['nome'];
+                    $_SESSION['tipo'] = $dados['tipo'];
+                    $_SESSION['mensagem'] = "Login efetuado com sucesso!";
+                    header('Location: ../users/teacher/index.php');
+                    break;
+                default:
+                    $_SESSION['mensagem'] = "Falha na leitura dos dados!";
+                    header('Location: ../index.php');
+                    break;
+            }
+        }else{
+            $_SESSION['mensagem']= "Senha ou login incorretos!";
             header('Location: ../index.php');
         }
     }else{
-        $_SESSION['mensagem']= "Erro ao cadastrar!";
+        $_SESSION['mensagem']= "Nenhuma informação foi recebida!";
         header('Location: ../index.php');
     }
 }

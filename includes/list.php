ddd471a919161260a5c1de6d7789815e71fb7a7e
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-06 15:07:36 -0300

    Adicionado listagem de usuários

--- includes/list.php
+++ includes/list.php
@@ -53,4 +53,41 @@
             echo '</form>';
         }
     }
+
+    function listusers(){
+        require_once '../../../includes/db_connection.php';
+        $sql = "SELECT * FROM usuario";
+        $resultado = mysqli_query($connect, $sql);
+        echo '<table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Login</th>
+                    <th>Data de Cadastro</th>
+                    <th>Nível</th>
+                    <th colspan="2">Ações</th>
+                </tr>
+            </thead>
+            <tbody>'; 
+            while($dados = mysqli_fetch_assoc($resultado)){   
+                $data = new DateTime($dados['dataCadastro']);
+                echo '<tr>';
+                echo '<form action="../../../includes/delete.php" method="post">';
+                echo '<input type="hidden" name="opcao" value="deletarUsuario">';
+                echo '<input type="hidden" name="id" value="' . $dados['idusuario'] . '">';
+                echo '<td>' . $dados['idusuario'] . '</td>';
+                echo '<td>' . $dados['nomeCompletoUsuario'] . '</td>';
+                echo '<td>' . $dados['login'] . '</td>';
+                echo "<td>" . $data->format('d/m/Y') . "</td>";
+                echo '<td>' . $dados['perfilUsuarioID'] . '</td>';
+                echo '<td><a href="../update/updateuser.php?id='. $dados['idusuario'] . '" >Editar</a>';
+                echo '<td><button type="submit" >Excuir</button>';
+                echo '</form>';
+                echo '</tr>';
+            }
+            echo '</tbody>
+        </table>';
+        
+    }
 ?>
--- index.php
+++ index.php
@@ -9,46 +9,19 @@
 </head>
 
 <body>
-    <!-- <header>
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="cadastro.html">Cadastro</a></li>
-                <li><a href="login.html">Login</a></li>
-            </ul>'
-        </nav>
-    </header> -->
     <main>
         <div>
-            <div>
-                <fieldset>
-                    <!-- TODO: Mover criação de usuário para função de adimim -->
-                    <form action="./login/createuser.php" method="post">
-                        <h2>Novo Usuário</h2>
-                        <label for="nome">Nome</label>
-                        <input type="text" name="nome" id="nome" placeholder="Nome" required ><br>
-                        <label for="login">Login</label>
-                        <input type="text" name="login" id="login" placeholder="Nome De Usuário" required ><br>
-                        <label for="senha">Senha</label>
-                        <input type="password" name="senha" id="senha" placeholder="Senha" required ><br>
-                        <input type="submit" value="Cadastrar" name="btn-cadastrar"><br><br>
-                    </form>
-                </fieldset>
-            </div>
-            <div>
-                <fieldset>
-                    <form action="./login/login.php" method="post">
-                        <h2>Login</h2>
-                        <label for="login">Login</label>
-                        <input type="text" name="login" id="login" placeholder="Nome de Usuário" required ><br>
-                        <label for="senha">Senha</label>
-                        <input type="password" name="senha" id="senha" placeholder="Senha" required ><br>
-                        <input type="submit" value="Entrar" name="btn-login"><br><br>
-                    </form>
-                </fieldset>
-            </div>
+            <fieldset>
+                <form action="./login/login.php" method="post">
+                    <h2>Login</h2>
+                    <label for="login">Login</label>
+                    <input type="text" name="login" id="login" placeholder="Nome de Usuário" required ><br>
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senha" placeholder="Senha" required ><br>
+                    <input type="submit" value="Entrar" name="btn-login"><br><br>
+                </form>
+            </fieldset>
         </div>
-        
     </main>
     
 </body>

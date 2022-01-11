e8c071a6acd219253f982ba787bdd098ffcc256e
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-11 13:23:50 -0300

    Adicionado atualização de imagens de tabuleiro

--- includes/update.php
+++ includes/update.php
@@ -99,6 +99,23 @@
             // header('Location: ../users/teacher/index.php');
         }
     }
+    function updateimageboard(){
+        require_once './db_connection.php';
+        $id = intval(mysqli_escape_string($connect, $_POST['id']));
+        $imagem = intval(mysqli_escape_string($connect, $_POST['imagem']));
+        $tabuleiro = intval(mysqli_escape_string($connect, $_POST['tabuleiro']));
+        $posicao = intval(mysqli_escape_string($connect, $_POST['posicao']));
+        $sql = "UPDATE tabuleiro_imagenstabuleiro SET tabuleiroID='$tabuleiro', imagenstabuleiroID='$imagem',  posicaoTabuleiro='$posicao' WHERE idtabuleiro_imagenstabuleiro = '$id';";
+
+        if(mysqli_query($connect, $sql)){
+            $_SESSION['mensagem']= "Atualizado com sucesso!";
+            header('Location: ../users/teacher/index.php');
+        }else{
+            $_SESSION['mensagem']= "Erro ao atualizar!";
+            echo mysqli_error($connect);
+            // header('Location: ../users/teacher/index.php');
+        }
+    }
 
     switch($_POST['opcao']){
         case 'atualizarNivel':
@@ -119,8 +136,11 @@
         case 'atualizarFundoTabuleiro':
             updatebackgroundboard();
             break;
+        case 'atualizarImagemTabuleiro':
+            updateimageboard();
+            break;
         default:
-        echo "Não foi possível realizar a operação!";
+            echo "Não foi possível realizar a operação!";
             break;
     }
 

a319cc92de55aabf5d186af8e8d851cb1003c517
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-11 13:33:35 -0300

    Adicionado exclusão de imagens de tabuleiro

--- includes/delete.php
+++ includes/delete.php
@@ -87,6 +87,20 @@
             // header('Location: ../users/teacher/index.php');
         }
     }
+    function deleteimageboard(){
+        require_once './db_connection.php';
+        $id = mysqli_escape_string($connect, $_POST['id']);
+        $sql = "DELETE FROM tabuleiro_imagenstabuleiro WHERE idtabuleiro_imagenstabuleiro = '$id';";
+
+        if(mysqli_query($connect, $sql)){
+            $_SESSION['mensagem']= "Deletado com sucesso!";
+            header('Location: ../users/teacher/index.php');
+        }else{
+            $_SESSION['mensagem']= "Erro ao deletar!";
+            echo mysqli_error($connect);
+            // header('Location: ../users/teacher/index.php');
+        }
+    }
 
     switch($_POST['opcao']){
         case 'deletarNivel':
@@ -106,6 +120,9 @@
             break;
         case 'deletarFundoTabuleiro':
             deletebackgroundboard();
+            break;
+        case 'deletarImagemTabuleiro':
+            deleteimageboard();
             break;
         default:
         echo "Não foi possível realizar a operação!";

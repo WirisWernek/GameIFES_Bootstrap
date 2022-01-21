2208f156bb5a6da7e17a0d2c325ac87b71c795d3
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-21 00:05:16 -0300

    modificado credenciais de acesso

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,12 +1,11 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password = "jinjoe0067";
-    $db = "softedu";
-    $connect = mysqli_connect($server, $user, $password, $db);
-    mysqli_set_charset($connect, "utf8");
+$server = "localhost";
+$user = "wiris";
+$password = "1+1Wiris1+1";
+$db = "softedu";
+$connect = mysqli_connect($server, $user, $password, $db);
+mysqli_set_charset($connect, "utf8");
 
-    if(mysqli_connect_error()) {
+if (mysqli_connect_error()) {
     echo "Erro na conexão: " . mysqli_connect_error();
-    }
-?>
+}

1e8423547b7f210881ee36c84cd2d4ff72d448ed
Wiris Rafael Januario Wernek <rafaeljwerneck43@gmail.com>
2022-03-01 11:05:10 -0300

    Teste de conexão pelo Heroku

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,7 +1,7 @@
 <?php
-$diretorio = $_SERVER['DOCUMENT_ROOT'] . '/includes/env.php';
-include_once $diretorio;
-$connect = mysqli_connect($server, $user, $password, $db);
+// $diretorio = $_SERVER['DOCUMENT_ROOT'] . '/includes/env.php';
+// include_once $diretorio;
+$connect = mysqli_connect("us-cdbr-east-05.cleardb.net", "b85cc0b2613008", "595391d3", "heroku_61081410bf8af24");
 mysqli_set_charset($connect, "utf8");
 
 if (mysqli_connect_error()) {
